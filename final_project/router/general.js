3299c4121c17ff794e6a36306c7e265a0c1c5176
AnkitSeal2023 <133688368+AnkitSeal2023@users.noreply.github.com>
2023-09-12 23:16:39 +0530

    Update general.js

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -27,10 +27,26 @@ public_users.get('/', function (req, res) {
 
 
 // Get book details based on ISBN
-public_users.get('/isbn/:isbn',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
- });
+public_users.get('/isbn/:isbn', function (req, res) {
+  const isbn = req.params.isbn;
+
+  if (!isbn) {
+    return res.status(400).json({ message: "ISBN is required" });
+  }
+
+  const book = Object.values(books).find(book => book.isbn === isbn);
+
+  if (!book) {
+    return res.status(404).json({ message: "Book not found" });
+  }
+
+  return res.status(300).json({
+    title: book.title,
+    author: book.author,
+    reviews: book.reviews
+  });
+});
+
   
 // Get book details based on author
 public_users.get('/author/:author',function (req, res) {
